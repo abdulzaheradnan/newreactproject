ae99359538b9d5f0a7d5904c6c1a76c714dc8f39
abdulzaheradnan <abdulzaheradnan143@gmail.com>
2023-02-12 01:01:59 +0530

    hello comment

--- src/interview.js
+++ src/interview.js
@@ -8,7 +8,7 @@ const name="adnan"
 const array=["a","a","b","b","c","d","e"];
 // adnan bahot acha hai
 // const data=[...new Set(array)]
-console.log("sorted array",data)
+// console.log("sorted array",data)
   return (
     <div className='con'>
 
