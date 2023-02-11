d353a7630ea3027c6b4db97fe135dfeb7255e22d
abdulzaheradnan <abdulzaheradnan143@gmail.com>
2023-02-12 01:05:55 +0530

    hello master bracmch

--- src/interview.js
+++ src/interview.js
@@ -7,8 +7,8 @@ import { TextField } from '@mui/material';
 const name="adnan"
 const array=["a","a","b","b","c","d","e"];
 
-const data=[...new Set(array)]
-console.log("sorted array",data)
+// const data=[...new Set(array)]
+// console.log("sorted array",data)
   return (
     <div className='con'>
 
